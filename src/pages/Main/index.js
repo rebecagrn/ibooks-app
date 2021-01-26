9f4b807a688762d237f7f1571fcd9accff79849d
Rebeca Pacheco <rbkgarner@gmail.com>
2021-01-26 19:22:49 -0300

    init project

---
+++ src/pages/Main/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import { FaPlus } from 'react-icons/fa';
+// import api from '../../services/api';
+
+import Container from '../../components/Container';
+import { MainTitle } from './styles';
+
+export default function Main() {
+  return (
+    <Container>
+      <MainTitle>
+        <h1>Books</h1>
+      </MainTitle>
+    </Container>
+  );
+}
---
+++ src/pages/Main/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const MainTitle = styled.div`
+  text-align: center;
+  display: flex;
+  justify-content: center;
+`;
